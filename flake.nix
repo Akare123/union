4d83d920eb30d74b7d1267545efd022bdb3b7384
Kaan Caglan <caglankaan@gmail.com>
2024-11-18 17:43:56 +0300

    chore (sui-ibc): added sui.nix file, it needs to be fixed
    Signed-off-by: Kaan Caglan <caglankaan@gmail.com>

--- flake.nix
+++ flake.nix
@@ -212,6 +212,7 @@
         ./lib/unionlabs/unionlabs.nix
         ./hubble/hubble.nix
         ./lib/aptos.nix
+        ./lib/sui.nix
         ./lib/ethereum-verifier/ethereum-verifier.nix
         ./lib/tendermint-verifier/tendermint-verifier.nix
         ./lib/scroll-verifier/scroll-verifier.nix
